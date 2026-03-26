1e1dc17c4d72553f19e9035e4fff822f28eb590e
Spamownia <spamownia91@gmail.com>
2026-03-26 20:13:16 +0100

    Update servers.js

--- config/servers.js
+++ config/servers.js
@@ -1,8 +1,6 @@
-// config/servers.js
 module.exports = [
   {
     NAME: "Husaria",
-    CFTOOLS_SERVER_API_ID: "7f3cb3e3-5575-4cd0-84e7-6b8e0c2af22a2",   // Server ID
-    CFTOOLS_CLOUD_ID: "69ba997375cb13ab97a0ad34"                      // Cloud ID (ważne!)
+    CFTOOLS_SERVER_API_ID: "7f3cb3e3-5575-4cd0-84e7-6b8e0c2af22a2"
   }
 ];
